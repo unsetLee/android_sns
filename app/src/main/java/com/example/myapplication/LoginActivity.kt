6dc54c4365fe67e060e40e1c2a49a3a042c1141a
wowon7975 <unset7975@gmail.com>
2019-01-24 18:42:33 +0900

    SNS 만들기 - 이메일 등록 function 구현 완료

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,12 +1,61 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    var auth : FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        email_login_button.setOnClickListener {
+            createAndLoginEmail()
+        }
+    }
+
+    fun createAndLoginEmail(){
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener{
+                task->
+                if(task.isSuccessful){
+                    moveMainPage(auth?.currentUser)
+                    Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_LONG).show()
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                } else {
+                signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail(){
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener{
+                task->
+                if(task.isSuccessful){
+                    moveMainPage(auth?.currentUser)
+                    Toast.makeText(this, "로그인이 완료되었습니다.", Toast.LENGTH_LONG).show()
+                }
+                else{
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?){
+        if(user!=null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 }
